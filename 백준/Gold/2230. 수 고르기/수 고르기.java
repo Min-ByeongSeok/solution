5ac80e6b764cc6bb4e7aad0f88e097344d70301d
Min-ByeongSeok <130335496+Min-ByeongSeok@users.noreply.github.com>
2023-07-11 12:48:25 +0900

    [Gold V] Title: 수 고르기, Time: 900 ms, Memory: 99664 KB -BaekjoonHub

---
+++ 백준/Gold/2230. 수 고르기/수 고르기.java
@@ -0,0 +1,42 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+    static Scanner sc = new Scanner(System.in);
+    static int N, M;
+    static int[] arr;
+
+    static void input(){
+        N = sc.nextInt();
+        M = sc.nextInt();
+
+        arr = new int[N];
+        for (int i = 0; i < N; i++) {
+            arr[i] = sc.nextInt();
+        }
+    }
+
+    static void process(){
+        Arrays.sort(arr);
+        int right = 0;
+        int ans = arr[N-1] - arr[0];
+
+        for (int left = 0; left < arr.length; left++) {
+
+            while(arr[right] - arr[left] < M && right+1 < arr.length){
+                right++;
+            }
+
+            int newSub = arr[right] - arr[left];
+            if(newSub >= M)
+                ans = Math.min(ans, newSub);
+        }
+
+        System.out.println(ans);
+    }
+
+    public static void main(String[] args) {
+        input();
+        process();
+    }
+}
