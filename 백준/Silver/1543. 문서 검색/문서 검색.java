08450887e6ced3be84f6951076b258a11cf342db
Min-ByeongSeok <130335496+Min-ByeongSeok@users.noreply.github.com>
2023-07-12 10:55:53 +0900

    [Silver IV] Title: 문서 검색, Time: 384 ms, Memory: 72620 KB -BaekjoonHub

---
+++ 백준/Silver/1543. 문서 검색/문서 검색.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Main {
+    static Scanner sc = new Scanner(System.in);
+    static String N, M;
+
+    static void input(){
+        N = sc.nextLine();
+        M = sc.nextLine();
+    }
+
+    static void process(){
+        String str = "";
+        for (int i = 0; i < N.length(); i++) {
+            str = N.replace(M, "");
+        }
+
+        int ans = (N.length() - str.length())/M.length();
+        System.out.println(ans);
+    }
+
+    public static void main(String[] args) {
+        input();
+        process();
+    }
+}
