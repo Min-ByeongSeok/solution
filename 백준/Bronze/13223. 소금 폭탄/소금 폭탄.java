917f2955f81f2f7e74926e1ca24f7a339bf9ea7d
Min-ByeongSeok <130335496+Min-ByeongSeok@users.noreply.github.com>
2023-07-17 12:00:07 +0900

    Update 소금 폭탄.java

--- 백준/Bronze/13223. 소금 폭탄/소금 폭탄.java
+++ 백준/Bronze/13223. 소금 폭탄/소금 폭탄.java
@@ -1,8 +1,9 @@
+package baekjoon.string;
+
 import java.util.Arrays;
 import java.util.Scanner;
-import java.util.stream.Collectors;
 
-public class Main {
+public class BOJ13223 {
     static Scanner sc = new Scanner(System.in);
     static String nowTime, soltTime;
     static int[] now, solt;
@@ -15,8 +16,6 @@ public class Main {
         solt = Arrays.stream(soltTime.split(":")).mapToInt(Integer::new).toArray();
     }
 
-    // 소금 14:36:22 04:00:00
-    // 현재 12:34:56 20:00:00
     static void process() {
         int[] ans = new int[3];
 
@@ -38,14 +37,26 @@ public class Main {
             solt[0] += 24;
         }
         ans[0] = solt[0] - now[0];
+
         if(ans[0] == 0 && ans[1] == 0 && ans[2] == 0)
             ans[0] = 24;
 
         System.out.printf("%02d:%02d:%02d", ans[0],ans[1],ans[2]);
     }
 
+    // 계층적으로 표현되는 각 단위를 계산할 때 가장 작은 단위로 통일하면 더 편할 수 있다.
+    static void process2(){
+        int nowSecond = now[0]*3600 + now[1]*60 + now[2];
+        int soltSecond = solt[0]*3600 + solt[1]*60 + solt[2];
+
+        int ans = soltSecond - nowSecond;
+        if(ans <= 0) ans += 24*3600;
+
+        System.out.printf("%02d:%02d:%02d", ans/3600, (ans%3600)/60, ans%60);
+    }
+
     public static void main(String[] args) {
         input();
-        process();
+        process2();
     }
 }
